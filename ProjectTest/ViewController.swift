a6fb57e84a41711abf605d0ba621bc8faf8697b5
Michael Nguyen-Kim <17828@cgs.act.edu.au>
2015-07-29 00:37:49 +1000

    Initial commit

---
+++ ProjectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectTest
+//
+//  Created by Michael Nguyen-Kim on 29/07/2015.
+//  Copyright (c) 2015 Michael Nguyen-Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
