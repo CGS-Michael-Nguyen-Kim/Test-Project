e73df2c081af55097bcb9d0641e2800c58072676
Michael Nguyen-Kim <17828@cgs.act.edu.au>
2015-07-30 15:02:27 +1000

    Second view controller

---
+++ ProjectTest/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  ProjectTest
+//
+//  Created by Michael Nguyen-Kim on 30/07/2015.
+//  Copyright (c) 2015 Michael Nguyen-Kim. All rights reserved.
+//
+
+import Foundation
